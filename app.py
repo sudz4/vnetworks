b1043aeb04f04862ff3632d1c4d63f46827816f1
sudz4 <mwsuther@syr.edu>
2023-07-27 17:53:59 -0700

    tst

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
-import requests    # pip install requests  
+from flask import request
 
 app = Flask(__name__)
 login_manager = LoginManager()
