3860fb4ecf7d85fea41980e2d3f62cedb5d2de4d
sudz4 <mwsuther@syr.edu>
2023-07-27 18:53:31 -0700

    all

--- playground/test_flask.py
+++ playground/test_flask.py
@@ -1,6 +1,6 @@
 from flask import Flask
-app = Flask(__name__) # create's the an instance of the Flask class
+app = Flask(__name__) # crea's the an instance of the Flask class
 
-@app.route("/") # this sets the route to this page
+@app.route("/") # maps the url route to the following function
 def index():
     return "You don't know me son!"
