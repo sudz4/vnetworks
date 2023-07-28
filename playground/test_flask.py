4137079cbd8099f9a20d05081a190d39a9fa586a
sudz4 <mwsuther@syr.edu>
2023-07-27 18:50:58 -0700

    dtop

--- playground/test_flask.py
+++ playground/test_flask.py
@@ -1,5 +1,5 @@
 from flask import Flask
-app = Flask(__name__) # creaet's the an instance of the Flask class
+app = Flask(__name__) # create's the an instance of the Flask class
 
 @app.route("/") # this sets the route to this page
 def index():
